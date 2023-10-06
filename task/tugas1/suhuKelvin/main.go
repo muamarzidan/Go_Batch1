8dbe0e7af2d76418fccb26aa9efa214251aa0bab
muamarzidan <codeofomiru11@gmail.com>
2023-10-07 06:19:53 +0700

    feat: adjust some task

--- task/tugas1/suhuKelvin/main.go
+++ task/tugas1/suhuKelvin/main.go
@@ -5,13 +5,14 @@ import (
 	"math"
 )
 
+//soal no 6&7 sama
 func main () {
 	var celcius, kelvin float64
-	fmt.Print("Suhu dalam celcius : ")
+	fmt.Print("Masukan suhu dalam celcius : ")
 	fmt.Scan(&celcius)
 
 	kelvin = celcius + 273.15
 	kelvin = math.Round(kelvin * 10) / 10
 
-	fmt.Println("Hasil convert : ", kelvin)
+	fmt.Println("Hasil convert menjadi satuan Kelvin adalah :", kelvin, "derajat")
 }
