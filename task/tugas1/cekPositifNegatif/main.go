f24191aa94977a6c4a6dbf729fbc8477440096bc
muamarzidan <codeofomiru11@gmail.com>
2023-10-07 05:42:20 +0700

    feat: add hurufKonsonan

--- task/tugas1/cekPositifNegatif/main.go
+++ task/tugas1/cekPositifNegatif/main.go
@@ -35,20 +35,7 @@ func main() {
 
 
 
-// 12
-// func main() {
-// 	var karakter string
 
-// 	fmt.Print("Masukkan huruf : ")
-// 	fmt.Scan(&karakter)
-// 	karakter = strings.ToLower(karakter)
-
-// 	if karakter != "a" && karakter != "e" && karakter != "i" && karakter != "o" && karakter != "u" {
-// 		fmt.Println("true")
-// 	} else {
-// 		fmt.Println("false")
-// 	}
-// }
 
 // 13
 // func main() {
---
+++ task/tugas1/hurufKonsonan/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+func main() {
+	var karakter string
+
+	fmt.Print("Masukkan huruf : ")
+	fmt.Scan(&karakter)
+	karakter = strings.ToLower(karakter)
+
+	if karakter != "a" && karakter != "e" && karakter != "i" && karakter != "o" && karakter != "u" {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+}
