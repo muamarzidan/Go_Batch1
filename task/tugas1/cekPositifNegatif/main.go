0c6d22cf9e15539516d7dfb22afacaccb9174e07
muamarzidan <codeofomiru11@gmail.com>
2023-10-07 05:41:02 +0700

    feat: add hurufVokal

---
+++ task/tugas1/cekPositifNegatif/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	// "strings"
+)
+
+//9
+// func main() {
+//     var bilangan int
+
+//     fmt.Print("Masukkan bilangan bulat: ")
+//     fmt.Scan(&bilangan)
+
+//     if bilangan < 0 {
+//         fmt.Println("true")
+//     } else {
+//         fmt.Println("false")
+//     }
+// }
+
+//10
+func main() {
+    var bilangan int
+
+    fmt.Print("Masukkan bilangan bulat: ")
+    fmt.Scan(&bilangan)
+
+    if bilangan < 0 {
+        fmt.Println("false")
+    } else {
+        fmt.Println("true")
+    }
+}
+
+
+
+// 12
+// func main() {
+// 	var karakter string
+
+// 	fmt.Print("Masukkan huruf : ")
+// 	fmt.Scan(&karakter)
+// 	karakter = strings.ToLower(karakter)
+
+// 	if karakter != "a" && karakter != "e" && karakter != "i" && karakter != "o" && karakter != "u" {
+// 		fmt.Println("true")
+// 	} else {
+// 		fmt.Println("false")
+// 	}
+// }
+
+// 13
+// func main() {
+// 	var bilangan int
+
+// 	fmt.Print("Masukkan sebuah bilangan bulat: ")
+// 	fmt.Scan(&bilangan)
+
+// 	if bilangan == 0 {
+// 		fmt.Println("true")
+// 	} else {
+// 		fmt.Println("false")
+// 	}
+// }
+
+// 14
+// func main() {
+// 	var nama string
+
+// 	fmt.Print("Masukkan kata : ")
+// 	fmt.Scan(&nama)
+
+// 	fmt.Println("Hai " + nama + ", saya sedang belajar Golang")
+// }
+
+//15
+// func main() {
+//     var karakter byte
+//     fmt.Print("Masukkan karakter: ")
+// 	fmt.Scanf("%c", &karakter)
+
+
+//     fmt.Printf("Karakter yang dimasukkan adalah: %c\n", karakter)
+// }
+
+
