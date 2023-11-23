c803f14a77f38416911e9f5f89dd9355b3613350
muamarzidan <codeofomiru11@gmail.com>
2023-11-24 00:59:59 +0700

    feat: refactor dataType

---
+++ dataType/index.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	// typeInt()
+	// typeFloat()
+	// typeString()
+	// typeChar()
+	// typeRune()
+	// typeBoolean()
+}
+
+// func typeInt() {
+// 	var tipeINT int // int is have 32 and 64 bit, and only for bilangan bulat
+// 	tipeINT = 4
+// 	fmt.Println(tipeINT)
+// }
+
+// func typeFloat() {
+// 	var tipeFLOAT float64 // float64 is have 32 and 64 bit, espicially for bilangan desimal/real
+// 	tipeFLOAT = 2.5
+// 	fmt.Println(tipeFLOAT)
+// }
+
+// func typeString() {
+// 	var tipeSTRING string // string is for text
+// 	tipeSTRING = "John Lennon"
+// 	fmt.Println(tipeSTRING)
+// }
+
+// func typeChar() {
+// 	var tipeCHAR byte // byte is for character (ASCII)
+// 	tipeCHAR = 'A'
+// 	fmt.Printf("%c\n", tipeCHAR)
+// }
+
+// func typeRune() {
+// 	var tipeRUNE rune // rune is for character, but rune can be used for unicode
+// 	typeRUNE = 'A'
+// 	fmt.Printf("%c\n", typeRUNE)
+// }
+
+// func typeBoolean() {
+// 	var KK bool // bool is for true or false, the default value is false
+// 	KK = true
+// 	fmt.Println(KK)
+// }
+
+
