9b9d2d33e61e8179c233ec6f5b118bc2767676ca
muamarzidan <codeofomiru11@gmail.com>
2023-11-24 01:59:02 +0700

    feat: refactor inputoutput

--- inputOutput/main.go
+++ inputOutput/main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	var a int
-	fmt.Scanf("%d", &a)
-	
+	fmt.Scan(&a) // scan is for normla input
+	fmt.Scanf("%d", &a) // scanf is for input with format
+	fmt.Scanln(&a) // scanln is for input with new line
+
 	fmt.Println(a) // println is for print with new line
 	fmt.Print(a)   // print is for print without new line
 	fmt.Printf("%d", a) // printf is for print with format
