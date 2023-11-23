9090d262ddb4572daff470039e6447f09362bf09
muamarzidan <codeofomiru11@gmail.com>
2023-11-24 02:02:20 +0700

    feat: refactor inputoutput

--- inputOutput/main.go
+++ inputOutput/main.go
@@ -11,4 +11,16 @@ func main() {
 	fmt.Println(a) // println is for print with new line
 	fmt.Print(a)   // print is for print without new line
 	fmt.Printf("%d", a) // printf is for print with format
+
+	var tipeint int
+	fmt.Scanf("%d", &tipeint)
+	fmt.Printf("%d\n", tipeint) // %d is for int
+
+	var tipefloat float64
+	fmt.Scanf("%f", &tipefloat)
+	fmt.Printf("%f\n", tipefloat) // %f is for float
+
+	var tipeByte byte
+	fmt.Scanf("%c", &tipeByte)
+	fmt.Printf("%c\n", tipeByte) // %c is for char or byte or rune
 }
