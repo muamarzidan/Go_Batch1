3bb3eff8bfcedbf3b8c000a8677c2abdafb70983
muamarzidan <codeofomiru11@gmail.com>
2023-12-01 08:56:01 +0700

    feat: materi boolean

---
+++ Latihan/Percabangan/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N, Mobil int;
+	fmt.Scan(&N);
+
+	Mobil = 7;
+	
+}
--- Latihan/boolean/main.go
+++ Latihan/boolean/main.go
@@ -3,24 +3,58 @@ package main
 import "fmt"
 
 func main() {
-	// var k byte
-	// var cekK bool
-	// fmt.Scan(&k)
+	//soal 1
+	// var k byte;
+	// var cekK bool;
+	// fmt.Scan(&k);
 
-	// cekK = (k >= 'a' && k <= 'z') || (k >= 'A' && k <= 'Z') || (k >= '0' && k <= '9')
-	// fmt.Println(cekK)
+	// cekK = (k >= 0 && k <= 9) || (k >= 'a' && k <= 'z') || (k >= 'A' && k <= 'Z');
+	// fmt.Println(cekK);
 
-	// var tahun int
-	// var cekTahun bool
-	// fmt.Scanln(&tahun)
 
-	// cekTahun = (tahun%4 == 0 && tahun%100 != 0) || (tahun%400 == 0) 
-	// fmt.Println(cekTahun)
+	//soal 2
+	// var tahun int;
+	// var cekTahun bool;
+	// fmt.Scan(&tahun);
 
-	var sudutA, sudutB, sudutC int
-	var cekSegitiga bool
-	fmt.Scanln(&sudutA, &sudutB, &sudutC)
+	// cekTahun = (tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0);
+	// fmt.Print(cekTahun);
 
-	cekSegitiga = sudutA+sudutB > sudutC && sudutA+sudutC > sudutB && sudutB+sudutC > sudutA
-	fmt.Println(cekSegitiga)
+
+	//soal 3
+	// var sudutA, sudutB, sudutC int;
+	// var cekSegitiga bool;
+	// fmt.Scanln(&sudutA, &sudutB, &sudutC);
+
+	// cekSegitiga = sudutA+sudutB > sudutC && sudutA+sudutC > sudutB && sudutB+sudutC > sudutA;
+	// fmt.Println(cekSegitiga);
+
+
+	//soal 4
+	// var total int
+    // var inginKartu, dapatKartu, dapatDiskon, dapatCashback, Diskon, Cashback bool
+
+    // fmt.Scan(&total)
+    // fmt.Scan(&inginKartu)
+
+    // Diskon = total >= 100000
+    // Cashback = total >= 200000 && inginKartu
+
+    // dapatKartu = inginKartu
+    // dapatDiskon = Diskon
+    // dapatCashback = Cashback
+
+    // fmt.Println("Kartu?", dapatKartu)
+    // fmt.Println("Diskon?", dapatDiskon)
+    // fmt.Println("Cashback?", dapatCashback)
+
+
+	//soal 5
+	var a, b, c int;
+	var cek bool;
+
+	cek = true;
+
+	cek = (a + b) / 2 == c || (a + c) / 2 == b || (b + c) / 2 == a;
+	fmt.Println(cek);
 }
