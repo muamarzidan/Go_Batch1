adc2252525f73da59f72702e7ec297271c38c095
muamarzidan <codeofomiru11@gmail.com>
2023-12-21 23:51:42 +0700

    feat: add soal Tipe Data Analisis

--- Latihan/tipeDataAnalisis/main.go
+++ Latihan/tipeDataAnalisis/main.go
@@ -8,5 +8,79 @@ func main() {
     fmt.Println("Kelas : IF-47-11")
     fmt.Println("Jawaban soal ppt Tipe Data dan Analisis")
 
+    //Soal1 Permukaan Bola
+    // var jariJari, phi, luasBola float64
 
+    // fmt.Print("Masukkan jari-jari bola : ")
+    // fmt.Scan(&jariJari)
+
+    // phi = 22.0 / 7.0
+    // luasBola = 4 * phi * (jariJari * jariJari)
+
+    // fmt.Print(luasBola)
+    // //make pseudo code
+    //program PermukaanBola
+    //kamus
+    //jariJari, phi, luasBola : float
+    //algoritma
+    //input(jariJari)
+    //phi <- 22/7
+    //luasBola <- 4 * phi * (jariJari * jariJari)
+    //output(luasBola)
+    //endprogram
+
+    //soal2 DigitGanda
+    // var angkaDuaDigit, d1, d2 int
+
+    // fmt.Scan(&angkaDuaDigit)
+
+    // d1 = angkaDuaDigit / 10
+    // d2 = angkaDuaDigit % 10
+    // fmt.Println(d1, d1, d2, d2)
+    //make pseudo code
+    //program DigitGanda
+    //kamus
+    //angkaDuaDigit, d1, d2 : integer
+    //algoritma
+    //input(angkaDuaDigit)
+    //d1 <- angkaDuaDigit / 10
+    //d2 <- angkaDuaDigit % 10
+    //output(d1, d1, d2, d2)
+    //endprogram
+
+    //soal3 Kapital
+    // var cekHuruf byte
+    // var kapital bool
+
+    // kapital = true
+    // fmt.Scanf("%c", &cekHuruf)
+    // kapital = 'A' <= cekHuruf && cekHuruf <= 'Z'
+    // fmt.Println(kapital)
+    // //make pseudo code
+    //program Kapital
+    //kamus
+    //cekHuruf : char
+    //kapital : boolean
+    //algoritma
+    //input(cekHuruf)
+    //kapital <- true
+    //kapital <- 'A' <= cekHuruf && cekHuruf <= 'Z'
+    //output(kapital)
+    //endprogram
+
+    //soal4 Energi
+    // var E0, c, E1, cc int
+    // fmt.Scan(&E0, &c, &E1)
+
+    // cc = (E0 - E1) / c
+    // fmt.Println(cc)
+    //make pseudo code
+    //program Energi
+    //kamus
+    //E0, c, E1, cc : integer
+    //algoritma
+    //input(E0, c, E1)
+    //cc <- (E0 - E1) / c
+    //output(cc)
+    //endprogram
 }
