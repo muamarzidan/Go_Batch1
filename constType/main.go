1acb8b89ae37b0676cd2c6d11fe389462ec62070
muamarzidan <codeofomiru11@gmail.com>
2024-02-20 09:30:22 +0700

    formatting

--- constType/main.go
+++ constType/main.go
@@ -4,17 +4,19 @@ import "fmt"
 
 func constInt() {
 	// jensi varibel
-	const MAXTINGGI int = 165
-	var tinggi int 
+	const jenis1 int = 165 // this is constant variable that cannot be changed
+	var jenis2 int // this is variable that can be changed
 
 	// penulisan varibel dalam go
-	// var saya1 string
-	// saya1 = "John Lennon"
-	// var saya2 string = "John Lennon"
-	// saya3 := "John Lennon"
+	var saya1 string
+	saya1 = "John Lennon"
+
+	var saya2 string = "John Lennon"
+
+	saya3 := "John Lennon"
 
 
-	fmt.Println(MAXTINGGI - tinggi) // the result is 0, cause the value of var tinggi is 170, not 165 
+	fmt.Println(saya1, saya2, saya3, jenis1, jenis2) 
 }
 
 
