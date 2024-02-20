4e2765e1a927b3255487d363ba64cf2149c6ecdd
muamarzidan <codeofomiru11@gmail.com>
2024-02-20 09:34:44 +0700

    formatting

--- loop/main.go
+++ loop/main.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println("Hello, world!")
-	fmt.Println("Hello, world!")
-	fmt.Println("Hello, world!")
-	fmt.Println("Hello, world!")
+	// this is a for loop code
+	// jadi for loop itu, program akan berjalan selama kondisi yang ditentukan benar
+	const n = 5
+	for i := 0; i < n; i++ {
+		fmt.Println(i)
+	}
+
+	//this is a while loop code
+	// jadi kondisi while itu selama kondisi awalnya true, maka akan berjalan, dan akan berhenti jika sudah
+	// tidak memenuhi kondisi awal
+	var tampung int = 2
+	for tampung < 10 {
+		fmt.Println(tampung)
+		tampung++
+	}
 }
